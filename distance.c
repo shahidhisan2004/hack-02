2e7c560236ab4de939611c0b81bcf9c2fa0be2dc
shahidhisan2004 <shahidhisan2004@gmail.com>
2023-10-24 09:05:05 -0700

    declaring variables

--- distance.c
+++ distance.c
@@ -5,4 +5,14 @@
 
 double degreesToRadians(double degrees) {
     return degrees * M_PI / 180.0;}
+    int main() {
+    double latitude1, longitude1, latitude2, longitude2;
+
+    const float  EARTH_RADIUS = 6371.0;
+
+    printf("Enter the latitude and longitude of the origin (in degrees):\n");
+    printf("Latitude: ");
+    scanf("%lf", &latitude1);
+    printf("Longitude: ");
+    scanf("%lf", &longitude1);
     
